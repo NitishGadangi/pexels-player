3b283298295e4e539d039239f665f1d27ec157f8
Nitish Gadangi <nitishgadangi@gmail.com>
2026-03-22 14:45:50 +0530

    Major: barebones

---
+++ PexelsPlayer/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  PexelsPlayer
+//
+//  Created by Nitish Gadangi on 3/22/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // dummy setup
+        view.backgroundColor = .red
+    }
+}
